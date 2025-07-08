e7445a543dc91531350d0d770aafaaa4a0ae1045
Nattakamol Chuejuntuk <nattakamol.c@ku.th>
2025-07-08 15:00:03 +0700

    lab01.py

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,14 @@
+# Program to ask for user's full name, age, phone number, and favorite hobby
+
+full_name = input("Enter your full name: ")
+age = input("Enter your age: ")
+email = input("Enter your email:")
+phone_number = input("Enter your phone number: ")
+favorite_hobby = input("Enter your favorite hobby: ")
+
+print("\n--- Your Profile ---")
+print("Full Name: " + full_name)
+print("Age:",age)
+print("Email : ",email)
+print("Phone Number:",phone_number)
+print("Favorite Hobby : ",favorite_hobby)
